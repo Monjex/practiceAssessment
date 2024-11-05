36efbb62a7dc26aa26f8f51d9c7b3406c4f5f002
Monjex <cse.engineermohit@gmail.com>
2024-11-05 15:18:18 +0530

    Initial Commit

---
+++ practiceAssignment/BottomCVC.swift
@@ -0,0 +1,17 @@
+//
+//  BottomCVC.swift
+//  practiceAssignment
+//
+//  Created by TGPL-MACMINI-66 on 06/11/24.
+//
+
+import UIKit
+
+class BottomCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
